fdf15909deb656cffc814769bad73afa10ff2c35
Roman <you@example.com>
2021-09-15 22:16:07 +0300

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 System.out.println("A");
-        System.out.println("Hello, World!1");
+        System.out.println("Hello, World!2");
  System.out.println("B");
     }
 }
